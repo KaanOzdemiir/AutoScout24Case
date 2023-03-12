9e8aae9e3a7b39a5d87b9a17e6750c1266603781
Kaan Ozdemir <kaanozdemir@teknasyon.com>
2023-03-12 21:26:52 +0300

    Initial Commit

---
+++ AutoScout24Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoScout24Case
+//
+//  Created by Kaan Ozdemir on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
